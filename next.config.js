fa253c3e931267d7dcf416491b43d656407285ce
BenXu <xuqiang056@gmail.com>
2024-02-20 00:19:57 +0800

    streaming

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(new MiniCssExtractPlugin());
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig;
